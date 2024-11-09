a9d1a6d9e5d963298b06a3cbb38b91f39bf9d0cb
MuhammadSamad30 <romivlogs11@gmail.com>
2024-11-10 00:56:11 +0500

    Portfolio-Next.js

---
+++ components/Footer.tsx
@@ -0,0 +1,29 @@
+
+const Footer = () => {
+    return (
+      <div id="footer-container">
+      <div id="footer">
+        <div>
+          <h2 id="footer-name">Muhammad Samad</h2>
+        </div>
+        <div id="footerLinks"> 
+          <a
+            href="https://github.com/MuhammadSamad30?tab=repositories"
+            target="_blank"
+            rel="noopener noreferrer">
+
+            <i className='bx bxl-github'></i> GitHub
+          </a>
+          <a
+            href="https://www.linkedin.com/in/muhammad-samad11/"
+            target="_blank"
+            rel="noopener noreferrer">
+            <i className='bx bxl-linkedin-square'></i> LinkedIn
+          </a>
+        </div> 
+      </div>
+    </div>
+    );
+  }
+  
+  export default Footer;
